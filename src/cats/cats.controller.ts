c0ba0eb725d64c07bff80c59243031d4ef91c5e8
CaioLFreitas98 <caio.freitas2598@gmail.com>
2022-08-09 14:26:30 -0400

    segundo get funcionando

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -3,7 +3,16 @@ import { Controller, Get } from '@nestjs/common';
 @Controller('cats')
 export class CatsController {
     @Get()
-    findAll():string{
-        return" estou ouovindo cats";
-    }
+        findAll():string{
+        return" estou ouvindo cats...";
+        }
+    
+    @Get('nome')
+        create(): string {
+        return 'seu nome é bichano';
+        }
+        
 }
+
+
+
